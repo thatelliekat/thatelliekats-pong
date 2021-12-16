b49ea06101e22619ba2bcdc11dd9370ed787c363
thatelliekat <96074425+thatelliekat@users.noreply.github.com>
2021-12-16 16:19:06 -0600

    Update pong-audio.js

--- Muag4200_Pong-main/javascript/pong-audio.js
+++ Muag4200_Pong-main/javascript/pong-audio.js
@@ -8,7 +8,7 @@ class soundFile {
   constructor(file, deferPlay) {
     //this.deferPlay = false;
     this.player = new Tone.Player({
-      url: "https://thatelliekat.github.io/thatelliekats-pong/Muag4200_Pong-main/sounds/" + file,
+      url: "https://github.com/thatelliekat/thatelliekats-pong/tree/main/Muag4200_Pong-main/sounds" + file,
       loop: false,
       autostart: false
     }).toMaster();
